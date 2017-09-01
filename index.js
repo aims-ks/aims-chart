8e979123a05e9ce9d592320e8cd5bc80d8356130
Aaron Smith <asmith@aims.gov.au>
2017-09-01 13:33:11 +1000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Package of Chart related functionality.
+ *
+ * @module aims-chart
+ */
+
+// Make classes available.
+import BasicChartView from './lib/basic-chart-view';
+export { BasicChartView };
+
+import LineChartRenderer from './lib/renderers/line-chart-renderer';
+export { LineChartRenderer };
